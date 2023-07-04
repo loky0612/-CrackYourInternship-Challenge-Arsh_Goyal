6fb6c809cdcf4aa6da72561dd8502449e9b2c163
LOKESH M <lokeshmalairaj@gmail.com>
2023-07-04 22:36:53 +0530

    commit

---
+++ Move Zeroes.py
@@ -0,0 +1,8 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        count = nums.count(0)
+        for i in range(count):
+            nums.remove(0)
+        for i in range(count):
+            nums.append(0)
+         
